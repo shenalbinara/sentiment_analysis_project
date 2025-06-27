7216f6d1caa67b57c0beac1a995df1e3fe8676bc
binara shenal <kahadhugodakankanamge.com>
2025-06-27 12:59:03 +0530

    build web app

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from helper import preprocessing, vectorizer_text, get_prediction
+
+
+
+app = Flask(__name__)
+
+
+data = dict()
+reviews = []
+positive = 0
+negative = 0
+
+
+@app.route("/")
+def index():
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+    return render_template('index.html', data=data)
+
+
+
+@app.route("/", methods=['post'])
+def my_post():
+    global positive, negative  # Declare global at top
+
+    text = request.form['text']
+    preprocessed_txt = preprocessing(text)
+    vectorized_txt = vectorizer_text(preprocessed_txt)
+    prediction = get_prediction(vectorized_txt)
+    
+    if prediction == 'Negative':
+        negative += 1
+    else:
+        positive += 1
+        
+    reviews.insert(0, text)
+    return redirect(request.url)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
